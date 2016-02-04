bad56a393a6bfd5928e5899a96dfddd1d64e45f6
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-02-04 10:22:47 -0600

    Everything is working except the mirage stuff....

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 
-
 export default Ember.Route.extend({
   model() {
-      return this.store.findAll('rental');
-    },
+    return this.store.findAll('rental');
+  }
 });
