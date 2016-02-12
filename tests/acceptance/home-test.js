4c95eb55601c1df5c746c5a1afbe95e2e49a3238
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-02-12 10:17:46 -0600

    Finished acceptance tests, need to figure out integration tests

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -3,6 +3,7 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | home');
 
+//Positive Tests------------------------------------------------------------
 test('visiting /', function(assert) {
   visit('/');
 
@@ -11,10 +12,29 @@ test('visiting /', function(assert) {
   });
 });
 
+test('visiting about link', function(assert) {
+  visit('/');
+  click('.about-link');
+  
+  andThen(function(){
+    assert.equal(currentURL(), '/about');
+  });
+});
+
+test('visiting contact link', function(assert) {
+  visit('/');
+  click('.contact-link');
+  
+  andThen(function(){
+    assert.equal(currentURL(), '/contact');
+  });
+});
+
 test('search box', function(assert){
   visit('/');
   fillIn('input', 'Seattle');
   click('button.search');
+  
   andThen(() => assert.equal(find('h2').text(), 'Urban Living'));
 });
 
@@ -22,11 +42,72 @@ test('search box', function(assert){
   visit('/');
   fillIn('input', 'Portland');
   click('button.search');
+  
   andThen(function(){
     assert.equal(find('h2').text(), 'Downtown Charm');
   });
 });
 
+test('search box', function(assert){
+  visit('/');
+  fillIn('input', 'San Francisco');
+  click('button.search');
+  
+  andThen(function(){
+    assert.equal(find('h2').text(), 'Grand Old Mansion');
+  });
+});
+
+test('click show button', function(assert){
+  visit('/');
+  click('.show-button');
+  
+  andThen(function(){
+    assert.equal(find('img').length, 3);
+  });
+});
+
+test('click first show button', function(assert){
+  visit('/');
+  click('.show-button:first');
+  
+  andThen(function(){
+    assert.equal(find('img').length, 1);
+  });
+});
+
+test('click last show button', function(assert){
+  visit('/');
+  click('.show-button:last');
+  
+  andThen(function(){
+    assert.equal(find('img').length, 1);
+  });
+});
+
+test('click hide button', function(assert) {
+  visit('/');
+  click('.show-button');
+  
+  andThen(function(){
+    click('.hide-button');
+  });
+  
+  andThen(function(){
+    assert.equal(find('.show-button').length, 3);
+  });
+});
+
+//Negative Tests-------------------------------------------------------
+test('Search for Omaha, nothing should show', function(assert) {
+  visit('/');
+  fillIn('input', 'Omaha');
+  click('button.search');
+  
+  andThen(function(){
+    assert.equal(find('h2').length, 0);
+  });
+});
 
 
 
--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -6,19 +6,22 @@ moduleForComponent('rental-listing', 'Integration | Component | rental listing',
 });
 
 test('it renders', function(assert) {
+  assert.expect(1);
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
   this.render(hbs`{{rental-listing}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('p').text().trim(), 'Owner: Type: Standalone - Location: Number of bedrooms:');
 
   // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#rental-listing}}
-      template block text
-    {{/rental-listing}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  // this.render(hbs`
+  //   {{#rental-listing}}
+  //     template block text
+  //   {{/rental-listing}}
+  // `);
+  //
+  // assert.equal(this.$().text().trim(), 'template block text');
 });
+
+
